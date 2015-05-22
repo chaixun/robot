64ea52d1a48f8c4373f83fd3d4aed25c2c49d98f
chaixun <chaixun@sjtu.edu.cn>
2015-05-22 14:57:13 +0800

    suc control original

---
+++ robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hex/git/robot/Aris_Control_demo/Control.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/Control.cpp.o"
+  "/home/hex/git/robot/Aris_Control_demo/Gait.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/Gait.cpp.o"
+  "/home/hex/git/robot/Aris_Control_demo/Hexapod_Robot.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/Hexapod_Robot.cpp.o"
+  "/home/hex/git/robot/Aris_Control_demo/Hexapod_Robot_Prmt.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/Hexapod_Robot_Prmt.cpp.o"
+  "/home/hex/git/robot/Aris_Control_demo/Server.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/Server.cpp.o"
+  "/home/hex/git/robot/Aris_Control_demo/main.cpp" "/home/hex/git/robot/Aris_Control_demo/CMakeFiles/Aris_Control_Demo.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/Lapack"
+  "/usr/include/Aris"
+  "/usr/xenomai/include"
+  "/opt/etherlab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
