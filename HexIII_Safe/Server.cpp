09cdac49d686d7210e44891eaca01dddbf6ded5f
chaixun <chaixun@sjtu.edu.cn>
2015-05-23 12:03:29 +0800

    HexIII_Safe add

---
+++ HexIII_Safe/Server.cpp
@@ -0,0 +1,224 @@
+#include "Server.h"
+
+CONN ControlSystem, VisionSystem;
+
+// CONN call back functions
+int On_CS_ConnectionReceived(Aris::Core::CONN *pConn, const char* addr,int port)
+{
+    Aris::Core::MSG msg;
+
+    msg.SetMsgID(CS_Connected);
+    msg.SetLength(sizeof(port));
+    msg.Copy(&port,sizeof(port));
+    msg.CopyMore(addr,strlen(addr));
+    PostMsg(msg);
+    return 0;
+}
+int On_CS_DataReceived(Aris::Core::CONN *pConn, Aris::Core::MSG &data)
+{
+    int cmd=data.GetMsgID();
+    MSG CMD=CS_CMD_Received;
+    CMD.SetLength(sizeof(int));
+    CMD.Copy(&cmd,sizeof(int));
+    cout<<"received CMD is "<<cmd<<endl;
+    PostMsg(CMD);
+    return 0;
+}
+int On_CS_ConnectionLost(Aris::Core::CONN *pConn)
+{
+    PostMsg(Aris::Core::MSG(CS_Lost));
+    return 0;
+}
+
+int OnGetControlCommand(MSG &msg)
+{
+    int CommandID;
+    msg.Paste(&CommandID,sizeof(int));
+    Aris::Core::MSG data;
+
+    switch(CommandID)
+    {
+    case 1:
+        data.SetMsgID(POWEROFF);
+        cs.NRT_PostMsg(data);
+        break;
+    case 2:
+        data.SetMsgID(STOP);
+        cs.NRT_PostMsg(data);
+        break;
+    case 3:
+        data.SetMsgID(ENABLE);
+        cs.NRT_PostMsg(data);
+        break;
+    case 4:
+        data.SetMsgID(RUNNING);
+        cs.NRT_PostMsg(data);
+        break;
+    case 5:
+        data.SetMsgID(GOHOME_1);
+        cs.NRT_PostMsg(data);
+        break;
+    case 6:
+        data.SetMsgID(GOHOME_2);
+        cs.NRT_PostMsg(data);
+        break;
+    case 7:
+        data.SetMsgID(HOME2START_1);
+        cs.NRT_PostMsg(data);
+        break;
+    case 8:
+        data.SetMsgID(HOME2START_2);
+        cs.NRT_PostMsg(data);
+        break;
+    case 9:
+        data.SetMsgID(FORWARD);
+        cs.NRT_PostMsg(data);
+        break;
+    case 10:
+        data.SetMsgID(BACKWARD);
+        cs.NRT_PostMsg(data);
+        break;
+    case 11:
+        data.SetMsgID(TURNLEFT);
+        cs.NRT_PostMsg(data);
+        break;
+    case 12:
+        data.SetMsgID(TURNRIGHT);
+        cs.NRT_PostMsg(data);
+        break;
+    case 13:
+        data.SetMsgID(LEGUP);
+        cs.NRT_PostMsg(data);
+        break;
+    case 14:
+        Aris::Core::PostMsg(Aris::Core::MSG(VS_Capture));
+        CGait::IsKeepSafeRegistered=true;
+        break;
+    default:
+        cout<<"Do Not Get Validate CMD"<<endl;
+        break;
+    }
+    return CommandID;
+}
+
+//MSG call back functions
+int On_CS_Connected(Aris::Core::MSG &msg)
+{
+    cout<<"Received Connection from Control System:"<<endl;
+    cout<<"   Remote IP is: "<<msg.GetDataAddress()+sizeof(int)<<endl;
+    cout<<"   Port is     : "<<*((int*)msg.GetDataAddress()) << endl << endl;
+
+    Aris::Core::MSG data(0,0);
+    ControlSystem.SendData(data);
+    return 0;
+}
+
+int On_CS_CMD_Received(Aris::Core::MSG &msg)
+{
+
+    MSG Command(msg);
+    Command.SetMsgID(GetControlCommand);
+    PostMsg(Command);
+    MSG data(0,0);
+    ControlSystem.SendData(data);
+    return 0;
+}
+
+int On_CS_Lost(Aris::Core::MSG &msg)
+{
+    cout << "Control system connection lost" << endl;
+    ControlSystem.StartServer("5690");
+    return 0;
+}
+
+int On_VS_ConnectionReceived(Aris::Core::CONN *pConn, const char* addr,int port)
+{
+    Aris::Core::MSG msg;
+    msg.SetMsgID(VS_Connected);
+    msg.SetLength(sizeof(port));
+    msg.Copy(&port,sizeof(port));
+    msg.CopyMore(addr,strlen(addr));
+    PostMsg(msg);
+    return 0;
+}
+
+int On_VS_DataReceived(Aris::Core::CONN *pConn, Aris::Core::MSG &data)
+{
+
+    cout<<"Vision Control Singnal Recieved"<<endl;
+    int controlcmd;
+    memcpy(&controlcmd,data.GetDataAddress(),data.GetLength());
+
+    Aris::Core::MSG command;
+    switch (controlcmd)
+    {
+    case 0:
+    {
+        Aris::Core::PostMsg(Aris::Core::MSG(VS_Capture));
+    }
+        break;
+    case 1:
+    {
+        command.SetMsgID(FORWARD);
+        cout<<"Send GAIT_MOVE Message to CS"<<endl;
+        cs.NRT_PostMsg(command);
+    }
+        break;
+    case 2:
+    {
+        command.SetMsgID(BACKWARD);
+        cout<<"Send GAIT_MOVE_BACK Message to CS"<<endl;
+        cs.NRT_PostMsg(command);
+    }
+        break;
+    case 3:
+    {
+        command.SetMsgID(TURNLEFT);
+        cout<<"Send GAIT_TURN_LEFT Message to CS"<<endl;
+        cs.NRT_PostMsg(command);
+    }
+        break;
+    case 4:
+    {
+        command.SetMsgID(TURNRIGHT);
+        cout<<"Send GAIT_TURN_RIGHT Message to CS"<<endl;
+        cs.NRT_PostMsg(command);
+    }
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+int On_VS_ConnectionLost(Aris::Core::CONN *pConn)
+{
+    CGait::IsKeepSafeRegistered = false;
+    PostMsg(Aris::Core::MSG(VS_Lost));
+    return 0;
+}
+
+int On_VS_Connected(Aris::Core::MSG &msg)
+{
+    cout<<"Received Connection from Vision System:"<<endl;
+    cout<<"   Remote IP is: "<<msg.GetDataAddress()+sizeof(int)<<endl;
+    cout<<"   Port is     : "<<*((int*)msg.GetDataAddress()) << endl << endl;
+
+    return 0;
+
+}
+
+int On_VS_Capture(Aris::Core::MSG &msg)
+{
+    MSG data(0,0);
+    VisionSystem.SendData(data);
+    return 0;
+}
+
+int On_VS_Lost(Aris::Core::MSG &msg)
+{
+    cout << "Visual system connection lost" << endl;
+    VisionSystem.StartServer("5691");
+    return 0;
+}
+
---
+++ HexIII_Safe/Server.h
@@ -0,0 +1,74 @@
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <iostream>
+#include <cstring>
+#include <Aris_Message.h>
+#include <Aris_Socket.h>
+#include "Gait.h"
+ 
+using namespace std;
+using namespace Aris::Core;
+
+extern Aris::RT_CONTROL::ACTUATION cs;
+
+enum Client_Msg
+{
+	CS_Connected=0,
+	CS_CMD_Received=1,
+	CS_Lost=2,
+
+    VS_Connected=3,
+    VS_Capture=4,
+    VS_Lost=5,
+
+};
+
+enum MACHINE_CMD
+{
+    NOCMD=1000,
+    POWEROFF=1001,
+    STOP=1002,
+    ENABLE=1003,
+    RUNNING=1004,
+    GOHOME_1=1005,
+    GOHOME_2=1006,
+    HOME2START_1=1007,
+    HOME2START_2=1008,
+    FORWARD=1009,
+    BACKWARD=1010,
+    LEGUP=1013,
+    TURNLEFT=1014,
+    TURNRIGHT=1015,
+};
+
+
+enum RobotCMD_Msg
+{
+	GetControlCommand=100,
+};
+
+//CS
+//MSG callback
+int On_CS_Connected(Aris::Core::MSG &msg);
+int On_CS_CMD_Received(Aris::Core::MSG &msg);
+int On_CS_Lost(Aris::Core::MSG &msg);
+int OnGetControlCommand(MSG &msg);
+
+int On_VS_Connected(Aris::Core::MSG &msg);
+int On_VS_Capture(Aris::Core::MSG &msg);
+int On_VS_Lost(Aris::Core::MSG &msg);
+
+// CONN callback
+int On_CS_ConnectionReceived(Aris::Core::CONN *pConn, const char* addr,int port);
+int On_CS_DataReceived(Aris::Core::CONN *pConn, Aris::Core::MSG &data);
+int On_CS_ConnectionLost(Aris::Core::CONN *pConn);
+
+int On_VS_ConnectionReceived(Aris::Core::CONN *pConn, const char* addr,int port);
+int On_VS_DataReceived(Aris::Core::CONN *pConn, Aris::Core::MSG &data);
+int On_VS_ConnectionLost(Aris::Core::CONN *pConn);
+
+extern Aris::Core::CONN ControlSystem;
+extern Aris::Core::CONN VisionSystem;
+
+#endif //SERVER_H_
